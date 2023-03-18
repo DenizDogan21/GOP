54484069b56700d4abcf6ff4bccc38d273ebc54e
DenizDogan21 <92920229+DenizDogan21@users.noreply.github.com>
2023-03-18 18:42:01 +0300

    update

--- lib/FrontEnd/Popup/canliVideolarPage.dart
+++ lib/FrontEnd/Popup/canliVideolarPage.dart
@@ -16,25 +16,11 @@ class _CanliVideolarPageState extends State<CanliVideolarPage> {
   Widget buildBackground() => background(context);
   Widget buildBottomNav() => bottomNav(context);
 
-  List imageList = [
-    {
-      "id": 1,
-      "image_path":
-          'https://www.gergitavan.net/wp-content/uploads/2019/01/S-131922689.jpg'
-    },
-    {
-      "id": 2,
-      "image_path":
-          'https://cdn.webrazzi.com/uploads/2020/05/instagram-canli-yayin-rozet-441.png'
-    },
-    {
-      "id": 3,
-      "image_path":
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Video_Camera_Live.svg/200px-Video_Camera_Live.svg.png'
-    },
+  final List<String> imageList = [
+    'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/6197v9EtYEL._AC_UF1000,1000_QL80_.jpg',
+    'https://st3.depositphotos.com/5739646/32145/v/1600/depositphotos_321453638-stock-illustration-basketball-tournament-modern-sports-posters.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Video_Camera_Live.svg/200px-Video_Camera_Live.svg.png',
   ];
-  final CarouselController carouselController = CarouselController();
-  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -47,57 +33,35 @@ class _CanliVideolarPageState extends State<CanliVideolarPage> {
         bottomNavigationBar: buildBottomNav(),
         body: Stack(children: [
           buildBackground(),
-          InkWell(
-            onTap: () {
-              print(currentIndex);
-            },
+          Center(
             child: CarouselSlider(
-              items: imageList
-                  .map(
-                    (item) => Image.asset(
-                      item['image_path'],
-                      fit: BoxFit.cover,
-                      width: double.infinity,
-                    ),
-                  )
-                  .toList(),
-              carouselController: carouselController,
               options: CarouselOptions(
-                scrollPhysics: const BouncingScrollPhysics(),
-                autoPlay: true,
-                aspectRatio: 2,
-                viewportFraction: 1,
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    currentIndex = index;
-                  });
-                },
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 10,
-            left: 0,
-            right: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: imageList.asMap().entries.map((entry) {
-                return GestureDetector(
-                  onTap: () => carouselController.animateToPage(entry.key),
-                  child: Container(
-                    width: currentIndex == entry.key ? 17 : 7,
-                    height: 7.0,
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 3.0,
-                    ),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: currentIndex == entry.key
-                            ? Colors.red
-                            : Colors.teal),
-                  ),
-                );
-              }).toList(),
+                  padEnds: false,
+                  scrollDirection: Axis.vertical,
+                  enableInfiniteScroll: false,
+                  autoPlay: false,
+                  height: double.maxFinite,
+                  viewportFraction: 1),
+              items: imageList
+                  .map((e) => Container(
+                          child: SizedBox(
+                        child: Stack(
+                          fit: StackFit.expand,
+                          children: <Widget>[
+                            IconButton(
+                              icon: Image.network(
+                                e,
+                                width: MediaQuery.of(context).size.width,
+                                fit: BoxFit.cover,
+                              ),
+                              onPressed: () {
+                                print("pressed the image");
+                              },
+                            ),
+                          ],
+                        ),
+                      )))
+                  .toList(),
             ),
           ),
         ]),
